77a0457e8552103e5367acb364a75ed65e0169c4
Dylan <dylantoyne@gmail.com>
2016-09-28 01:04:13 +0000

    Adding looping exercises

---
+++ ingredients.js
@@ -0,0 +1,29 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i = 0;
+while (i <= 0) {
+  console.log(ingredients);
+  i++;
+}
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+console.log("Things we need to bring to the picnic:");
+for (var i = 0; i <= 0; i++) {
+  console.log(ingredients);
+}
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+console.log("Ingredients backwards in a while loop:")
+var i = 7;
+while (i >= 0) {
+  console.log(ingredients[i]);
+  i--;
+}
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+console.log("The ingredients backwards in a for loop:");
+for (var i = 7; i >= 0; --i) {
+  console.log(ingredients[i]);
+}
+
+
+
--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,6 @@
 var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (var i = 0; i < packingList.length; i++) {
+for (var i = 7; i < packingList.length; i -= 1) {
   console.log(packingList[i]);
 }
